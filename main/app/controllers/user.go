c5781ed928406cbffdcd90bb070784636d240012
itang <live.tang@gmail.com>
2014-03-06 12:50:26 +0800

    update

--- main/app/controllers/user.go
+++ main/app/controllers/user.go
@@ -36,19 +36,19 @@ func (c User) ChangePassword() revel.Result {
 
 // 修改密码处理
 func (c User) DoChangePassword(oldPassword, password, confirmPassword string) revel.Result {
-	c.Validation.Required(oldPassword).Message("请输入旧密码")
+	c.Validation.Required(oldPassword).Message("请输入原始密码")
 	c.Validation.Required(password).Message("请输入新密码")
 	c.Validation.MinSize(password, 6).Message("请输入6位新密码")
 	c.Validation.MaxSize(password, 50).Message("输入新密码位数太长了")
 	c.Validation.Required(confirmPassword).Message("请输入确认新密码")
 	c.Validation.Required(password == confirmPassword).Message("两次输入的新密码不匹配").Key("confirmPassword")
-	c.Validation.Required(password != oldPassword).Message("输入了与旧密码相同的新密码").Key("password")
+	c.Validation.Required(password != oldPassword).Message("输入了与原始密码相同的新密码").Key("password")
 	if ret := c.doValidate(User.ChangePassword); ret != nil {
 		return ret
 	}
 
 	user := c.forceCurrUser()
-	c.Validation.Required(c.userService().VerifyPassword(user.CryptedPassword, oldPassword)).Message("你的旧密码输入有误").Key("oldPassword")
+	c.Validation.Required(c.userService().VerifyPassword(user.CryptedPassword, oldPassword)).Message("你的原始密码输入有误").Key("oldPassword")
 	if ret := c.doValidate(User.ChangePassword); ret != nil {
 		return ret
 	}
