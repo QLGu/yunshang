5306367556fedc5ab24b44040dbcdfe15386628a
itang <live.tang@gmail.com>
2014-03-09 11:55:54 +0800

    update

--- main/app/models/entity/entity.go
+++ main/app/models/entity/entity.go
@@ -1,12 +1,7 @@
 package entity
 
 import (
-	"encoding/json"
-	"fmt"
-	"os"
 	"time"
-
-	"github.com/itang/gotang"
 )
 
 type regionData map[string]string
@@ -17,35 +12,6 @@ func (e regionData) GetById(id string) string {
 		return ""
 	}
 	return v
-}
-
-var rd regionData = make(map[string]string)
-
-func init() {
-	file, err := os.Open("public/libs/city/city.json")
-	gotang.AssertNoError(err)
-	var data map[string]interface{}
-	err = json.NewDecoder(file).Decode(&data)
-	gotang.AssertNoError(err)
-
-	for pid, v := range data {
-		pData := v.(map[string]interface{})
-		rd[pid] = pData["name"].(string)
-		for cid, v := range pData["data"].(map[string]interface{}) {
-			cData := v.(map[string]interface{})
-			rd[pid+cid] = cData["name"].(string)
-			if _, ok := cData["data"].(map[string]interface{}); ok {
-				for did, v := range cData["data"].(map[string]interface{}) {
-					dData := v.(map[string]interface{})
-					rd[pid+cid+did] = dData["name"].(string)
-				}
-			} else {
-				for i, v := range cData["data"].([]interface{}) {
-					fmt.Printf("dd\t\t%s:%s\n", i, v)
-				}
-			}
-		}
-	}
 }
 
 // 用户等级
