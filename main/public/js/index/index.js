f58adf4daf1356961d5e87033a6addca5ee36ddf
itang <live.tang@gmail.com>
2014-03-19 23:50:08 +0800

    update

--- main/public/js/index/index.js
+++ main/public/js/index/index.js
@@ -21,13 +21,22 @@ $(function () {
                     ractive.images = images;
                     ractive.set("images", ractive.images);
                     ractive.set("curr", ractive.images[0]);
+                    ractive.index=0;
                 });
             },
             "selected": function (e, index) {
                 ractive.set("curr", ractive.images[index]);
+                ractive.index = index;
             }
         });
         ractive.fire("load");
+
+        setInterval(function(){
+            var len = ractive.images.length;
+            var index = (ractive.index + 1) % len;
+            ractive.set("curr", ractive.images[index]);
+            ractive.index = index;
+        }, 5000);
     })();
 });
 
