da03ae283704e3f5a29ce71ec95b0dde2886d5bb
itang <live.tang@gmail.com>
2014-04-03 10:41:34 +0800

    update

--- main/public/js/app.js
+++ main/public/js/app.js
@@ -1,6 +1,3 @@
-Ractive.delimiters = [ '[[', ']]' ];
-Ractive.tripleDelimiters = [ '[[[', ']]]' ];
-
 var _$captchaImgSrc = "";
 function freshCaptcha() {
     var $captchaImg = $("#captchaImg");
@@ -10,80 +7,7 @@ function freshCaptcha() {
     $captchaImg.attr("src", _$captchaImgSrc + "?time=" + new Date().getTime());
 }
 
-function _isDevAjax(ajaxOptions) {
-    return ajaxOptions["url"].indexOf("/@reveltang_dev/__source_changed") != -1;
-}
-
-function reloadWindow() {
-    window.location.reload();
-}
-
-function doAjaxPost(url, data, after) {
-    if (_.isFunction(data)) {
-        after = data;
-        data = {};
-    }
-    if (confirm("确认执行此操作？")) {
-        $.post(url, data, function (result) {
-            if (result.ok) {
-                alert("操作成功：" + result.message);
-                if (after) {
-                    after(result)
-                }
-            } else {
-                alert("操作失败：" + result.message);
-            }
-        }, "json");
-    }
-}
-
 $(function () {
-    // Ajax Global Config
-    $(document).ajaxSend(function (_event, _jqXHR, ajaxOptions) {
-        if (!_isDevAjax(ajaxOptions)) { // not dev ajax
-            if ($("#loadingbar").length === 0) {
-                $("body").append("<div id='loadingbar'></div>")
-                $("#loadingbar").addClass("waiting").append($("<dt/><dd/>"));
-
-                $("#loadingbar").width((50 + Math.random() * 30) + "%");
-            }
-        }
-    });
-    $.ajaxPrefilter(function (opt, origOpt, jqxhr) {
-        jqxhr.always(function () {
-            $("#loadingbar").width("101%").delay(200).fadeOut(400, function () {
-                $(this).remove();
-            });
-            //$("[data-plugin]").plugin();
-        });
-    });
-    /*
-     $(document).ajaxComplete(function (_event, _XMLHttpRequest, ajaxOptions) {
-     if (!_isDevAjax(ajaxOptions)) {
-     $("#loadingbar").width("101%").delay(200).fadeOut(400, function () {
-     $(this).remove();
-     });
-     }
-     });*/
-
-    // Ajaxable Links
-    $("a[data-url]").click(function () {
-        if (confirm("确认执行此操作？")) {
-            var $this = $(this)
-            $.post($(this).data("url"), function (result) {
-                if (result.ok) {
-                    alert("操作成功：" + result.message);
-                    var after = $this.data("after");
-                    if (after) {
-                        eval(after);
-                    }
-                } else {
-                    alert("操作失败：" + result.message);
-                }
-            }, "json");
-        }
-    });
-
     function doAjaxLogin() {
         $login = $('input[name=login]');
         $password = $('input[name=password]');
--- main/public/js/ie-warn.js
+++ main/public/js/ie-warn.js
@@ -2,7 +2,7 @@ if ($.browser.msie) {
     if ($.browser.version < 8.0) {
         if (!$.cookie('ie-warn')) {
             alert("检测您在使用低版本的IE浏览器访问本站。 为了提升您的使用体验，请使用IE8或以上版本，谢谢支持！");
-            $.cookie('ie-warn', true);
+            $.cookie('ie-warn', true, {expires: 1, path: '/' });
         }
     }
 }
